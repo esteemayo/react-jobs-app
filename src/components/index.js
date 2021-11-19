ebd00d4a23dd1affae553bd013d7d08c752f856c
Emmanuel Adebayo <eadebayo15@gmail.com>
2021-11-19 13:51:08 +0100

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+import FormRow from './FormRow';
+import Job from './Job';
+import Jobs from './Jobs';
+import JobColumns from './JobColumns';
+import Navbar from './Navbar';
+import Spinner from './Spinner';
+
+export { FormRow, Job, Jobs, JobColumns, Navbar, Spinner };

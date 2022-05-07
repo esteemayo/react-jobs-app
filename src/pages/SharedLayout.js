710929c3d0930801e57d029cf063eee38d7999ac
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-05-07 18:59:22 +0100

    moved Navbar into SharedLayout

--- src/pages/SharedLayout.js
+++ src/pages/SharedLayout.js
@@ -1,11 +1,17 @@
+import { useSelector } from 'react-redux';
 import { Outlet } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
+
+import Navbar from 'components/Navbar';
 
 import 'react-toastify/dist/ReactToastify.css';
 
 const SharedLayout = () => {
+  const { user } = useSelector((state) => state.user);
+
   return (
     <>
+      {user && <Navbar />}
       <ToastContainer />
       <Outlet />
     </>

0b098be3075f40ad9a32e1412f71037a3dfee9fb
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-05-07 16:22:21 +0100

    added SharedLayout

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,5 +3,6 @@ import Edit from './Edit';
 import Error from './Error';
 import Register from './Register';
 import Dashboard from './Dashboard';
+import SharedLayout from './SharedLayout';
 
-export { Edit, Error, Home, Dashboard, Register };
+export { Edit, Error, Home, Dashboard, Register, SharedLayout };

e96c49deb142e7f4a8cca5bd6a94229489e78b11
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-05-07 16:06:11 +0100

    refactored

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,39 +1,31 @@
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-import { Navigate } from 'react-router-dom';
 
 import logo from 'assets/logo.svg';
 import main from 'assets/main.svg';
 
 const Home = () => {
-  const { user } = useSelector((state) => state.user);
-
   return (
-    <>
-      {user && <Navigate to='/dashboard' />}
-      <Wrapper>
-        <nav>
-          <img src={logo} alt='jobs app' />
-        </nav>
-        <div className='container page'>
-          <div className='info'>
-            <h1>job tracking app</h1>
-            <p>
-              I'm baby viral enamel pin chartreuse cliche retro af selfies
-              kinfolk photo booth plaid jianbing actually squid 3 wolf moon
-              lumbersexual. Hell of humblebrag gluten-free lo-fi man braid
-              leggings.
-            </p>
+    <Wrapper>
+      <nav>
+        <img src={logo} alt='jobs app' />
+      </nav>
+      <div className='container page'>
+        <div className='info'>
+          <h1>job tracking app</h1>
+          <p>
+            I'm baby viral enamel pin chartreuse cliche retro af selfies kinfolk
+            photo booth plaid jianbing actually squid 3 wolf moon lumbersexual.
+            Hell of humblebrag gluten-free lo-fi man braid leggings.
+          </p>
 
-            <Link to='/register' className='btn hero-btn'>
-              Login / Register
-            </Link>
-          </div>
-          <img src={main} alt='job hunt' className='img main-img' />
+          <Link to='/register' className='btn hero-btn'>
+            Login / Register
+          </Link>
         </div>
-      </Wrapper>
-    </>
+        <img src={main} alt='job hunt' className='img main-img' />
+      </div>
+    </Wrapper>
   );
 };
 
@@ -44,6 +36,7 @@ const Wrapper = styled.div`
     align-items: center;
     margin-top: -3rem;
   }
+
   nav {
     width: var(--fluid-width);
     max-width: var(--max-width);
@@ -52,17 +45,21 @@ const Wrapper = styled.div`
     display: flex;
     align-items: center;
   }
+
   h1 {
     font-weight: 700;
   }
+
   .main-img {
     display: none;
   }
+
   @media (min-width: 992px) {
     .container {
       grid-template-columns: 1fr 1fr;
       column-gap: 6rem;
     }
+
     .main-img {
       display: block;
     }

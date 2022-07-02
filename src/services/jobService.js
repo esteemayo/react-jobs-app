69c4aaf4661cfb8a03ea13f81c8ef6849bb0b6fa
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-07-02 21:54:51 +0100

    modified

--- src/services/jobService.js
+++ src/services/jobService.js
@@ -2,30 +2,17 @@ import http from './httpService';
 
 const apiEndpoint = '/jobs';
 
-function jobUrl(jobId) {
-  return `${apiEndpoint}/${jobId}`;
-}
+const jobUrl = (jobId) => `${apiEndpoint}/${jobId}`;
 
-export function getJobs() {
-  return http.get(apiEndpoint);
-}
+export const getJobs = () => http.get(apiEndpoint);
 
-export function getJobById(jobId) {
-  return http.get(jobUrl(jobId));
-}
+export const getJobById = (jobId) => http.get(jobUrl(jobId));
 
-export function getJobBySlug(slug) {
-  return http.get(`${apiEndpoint}/details/${slug}`);
-}
+export const getJobBySlug = (slug) =>
+  http.get(`${apiEndpoint}/details/${slug}`);
 
-export function createJob(job) {
-  return http.post(apiEndpoint, job);
-}
+export const createJob = (job) => http.post(apiEndpoint, job);
 
-export function updateJob(jobId, job) {
-  return http.patch(jobUrl(jobId), job);
-}
+export const updateJob = (jobId, job) => http.patch(jobUrl(jobId), job);
 
-export function deleteJob(jobId) {
-  return http.delete(jobUrl(jobId));
-}
+export const deleteJob = (jobId) => http.delete(jobUrl(jobId));

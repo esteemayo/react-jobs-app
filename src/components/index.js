621bf16dbf937fcb087c9e8c2d420ed97111d38f
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-07-02 17:54:54 +0100

    deleted

--- src/components/index.js
+++
@@ -1,9 +0,0 @@
-import Alert from './Alert';
-import FormRow from './FormRow';
-import Job from './Job';
-import Jobs from './Jobs';
-import JobColumns from './JobColumns';
-import Navbar from './Navbar';
-import Spinner from './Spinner';
-
-export { Alert, FormRow, Job, Jobs, JobColumns, Navbar, Spinner };

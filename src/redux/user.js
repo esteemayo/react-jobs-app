4fa481ba5f2f4d61fd480acf72c557a74ef89018
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-07-02 21:02:07 +0100

    refactored

--- src/redux/user.js
+++ src/redux/user.js
@@ -3,6 +3,32 @@ import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 
 import * as userAPI from 'services/userService';
 
+export const loginUserAsync = createAsyncThunk(
+  'user/login',
+  async ({ user, toast }, { rejectWithValue }) => {
+    try {
+      const { data } = await userAPI.loginUser({ ...user });
+      toast.success('Login successful');
+      return data;
+    } catch (err) {
+      return rejectWithValue(err.response.data);
+    }
+  }
+);
+
+export const registerUserAsync = createAsyncThunk(
+  'user/register',
+  async ({ user, toast }, { rejectWithValue }) => {
+    try {
+      const { data } = await userAPI.registerUser({ ...user });
+      toast.success('Registration successful');
+      return data;
+    } catch (err) {
+      return rejectWithValue(err.response.data);
+    }
+  }
+);
+
 const initialState = {
   user: null,
   isFetching: false,
@@ -24,32 +50,6 @@ if (token) {
   }
 }
 
-export const registerUserAsync = createAsyncThunk(
-  'user/register',
-  async (user, { rejectWithValue }) => {
-    try {
-      const { data } = await userAPI.registerUser(user);
-      localStorage.setItem(tokenKey, data.token);
-      return data.user;
-    } catch (err) {
-      return rejectWithValue(err.response.data);
-    }
-  }
-);
-
-export const loginUserAsync = createAsyncThunk(
-  'user/login',
-  async (user, { rejectWithValue }) => {
-    try {
-      const { data } = await userAPI.loginUser(user);
-      localStorage.setItem(tokenKey, data.token);
-      return data.user;
-    } catch (err) {
-      return rejectWithValue(err.response.data);
-    }
-  }
-);
-
 export const userSlice = createSlice({
   name: 'user',
   initialState,
@@ -60,29 +60,31 @@ export const userSlice = createSlice({
     },
   },
   extraReducers: {
-    [registerUserAsync.pending]: (state) => {
-      state.isFetching = true;
-      state.showAlert = false;
-    },
-    [registerUserAsync.fulfilled]: (state, { payload }) => {
-      state.isFetching = false;
-      state.user = payload;
-    },
-    [registerUserAsync.rejected]: (state, { payload }) => {
-      state.isFetching = false;
-      state.user = null;
-      state.showAlert = true;
-      state.error = payload.message;
-    },
     [loginUserAsync.pending]: (state) => {
       state.isFetching = true;
       state.showAlert = false;
     },
     [loginUserAsync.fulfilled]: (state, { payload }) => {
       state.isFetching = false;
+      localStorage.setItem(tokenKey, payload.token);
       state.user = payload;
     },
     [loginUserAsync.rejected]: (state, { payload }) => {
+      state.isFetching = false;
+      state.user = null;
+      state.showAlert = true;
+      state.error = payload.message;
+    },
+    [registerUserAsync.pending]: (state) => {
+      state.isFetching = true;
+      state.showAlert = false;
+    },
+    [registerUserAsync.fulfilled]: (state, { payload }) => {
+      state.isFetching = false;
+      localStorage.setItem(tokenKey, payload.token);
+      state.user = payload;
+    },
+    [registerUserAsync.rejected]: (state, { payload }) => {
       state.isFetching = false;
       state.user = null;
       state.showAlert = true;

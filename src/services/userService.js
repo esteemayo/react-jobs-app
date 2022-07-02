9f94fddad58bb76450345370fdb55a4f17a352b0
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-07-02 22:00:35 +0100

    refcatored

--- src/services/userService.js
+++ src/services/userService.js
@@ -3,14 +3,10 @@ import http from './httpService';
 const tokenKey = 'jwt';
 const apiEndpoint = '/users';
 
-export function registerUser(userData) {
-  return http.post(`${apiEndpoint}/register`, userData);
-}
+export const registerUser = (userData) =>
+  http.post(`${apiEndpoint}/register`, userData);
 
-export function loginUser(userData) {
-  return http.post(`${apiEndpoint}/login`, userData);
-}
+export const loginUser = (userData) =>
+  http.post(`${apiEndpoint}/login`, userData);
 
-export function getJwt() {
-  return localStorage.getItem(tokenKey);
-}
+export const getJwt = () => localStorage.getItem(tokenKey);

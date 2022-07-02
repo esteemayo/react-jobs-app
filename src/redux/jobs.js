8d44bf938fc305ae911e0bc597639c6d6600f93e
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-07-02 18:25:11 +0100

    modified

--- src/redux/jobs.js
+++ src/redux/jobs.js
@@ -1,60 +1,88 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import * as jobService from 'services/jobService';
 
-export const fetchJobsAsync = createAsyncThunk('jobs/fetchJobs', async () => {
-  const {
-    data: { jobs },
-  } = await jobService.getJobs();
-  return jobs;
-});
+export const fetchJobsAsync = createAsyncThunk(
+  'jobs/fetchJobs',
+  async (_, { rejectWithValue }) => {
+    try {
+      const {
+        data: { jobs },
+      } = await jobService.getJobs();
+      return jobs;
+    } catch (err) {
+      return rejectWithValue(err.response.data);
+    }
+  }
+);
 
 export const fetchSingleJobBySlugAsync = createAsyncThunk(
   'jobs/fetchSingleJobBySlug',
-  async (slug) => {
-    const {
-      data: { job },
-    } = await jobService.getJobBySlug(slug);
-    return job;
+  async (slug, { rejectWithValue }) => {
+    try {
+      const {
+        data: { job },
+      } = await jobService.getJobBySlug(slug);
+      return job;
+    } catch (err) {
+      return rejectWithValue(err.response.data);
+    }
   }
 );
 
 export const createJobAsync = createAsyncThunk(
   'jobs/createJob',
-  async (job) => {
-    const {
-      data: { job: newJob },
-    } = await jobService.createJob(job);
-    return newJob;
+  async ({ newJob, toast }, { rejectWithValue }) => {
+    try {
+      const {
+        data: { job },
+      } = await jobService.createJob({ ...newJob });
+      toast.success('Job successfully created');
+      return job;
+    } catch (err) {
+      return rejectWithValue(err.response.data);
+    }
   }
 );
 
 export const updateJobAsync = createAsyncThunk(
   'jobs/updateJob',
-  async (jobId, job) => {
-    const {
-      data: { job: updJob },
-    } = await jobService.updateJob(jobId, job);
-    return updJob;
+  async ({ jobId, job }, { rejectWithValue }) => {
+    try {
+      const {
+        data: { job: updJob },
+      } = await jobService.updateJob(jobId, job);
+      return updJob;
+    } catch (err) {
+      return rejectWithValue(err.response.data);
+    }
   }
 );
 
 export const deleteJobAsync = createAsyncThunk(
   'jobs/deleteJob',
-  async (jobId) => {
-    await jobService.deleteJob(jobId);
+  async ({ jobId, toast }, { rejectWithValue }) => {
+    try {
+      await jobService.deleteJob(jobId);
+      toast.success('Job Deleted Successfully');
+    } catch (err) {
+      return rejectWithValue(err.response.data);
+    }
   }
 );
 
+const initialState = {
+  jobs: [],
+  isLoading: false,
+  showAlert: false,
+  editItem: null,
+  singleJobError: false,
+  editComplete: false,
+  error: '',
+};
+
 export const jobSlice = createSlice({
   name: 'jobs',
-  initialState: {
-    jobs: [],
-    isLoading: false,
-    showAlert: false,
-    editItem: null,
-    singleJobError: false,
-    editComplete: false,
-  },
+  initialState,
   reducers: {
     updateStart: (state) => {
       state.isLoading = true;
@@ -82,8 +110,9 @@ export const jobSlice = createSlice({
       state.isLoading = false;
       state.jobs = payload;
     },
-    [fetchJobsAsync.rejected]: (state) => {
+    [fetchJobsAsync.rejected]: (state, { payload }) => {
       state.isLoading = false;
+      state.error = payload.message;
     },
     [fetchSingleJobBySlugAsync.pending]: (state) => {
       state.isLoading = true;
@@ -94,10 +123,11 @@ export const jobSlice = createSlice({
       state.isLoading = false;
       state.editItem = payload;
     },
-    [fetchSingleJobBySlugAsync.rejected]: (state) => {
+    [fetchSingleJobBySlugAsync.rejected]: (state, { payload }) => {
       state.isLoading = false;
       state.editItem = '';
       state.singleJobError = true;
+      state.error = payload.message;
     },
     [createJobAsync.pending]: (state) => {
       state.isLoading = true;
@@ -108,27 +138,53 @@ export const jobSlice = createSlice({
       state.isLoading = false;
       state.jobs.unshift(payload);
     },
-    [createJobAsync.rejected]: (state) => {
+    [createJobAsync.rejected]: (state, { payload }) => {
       state.isLoading = false;
       state.showAlert = true;
+      state.error = payload.message;
     },
     [updateJobAsync.pending]: (state) => {
       state.isLoading = true;
       state.showAlert = false;
       state.editComplete = false;
     },
-    [updateJobAsync.fulfilled]: (state, { payload }) => {
+    [updateJobAsync.fulfilled]: (state, { meta, payload }) => {
       state.isLoading = false;
       state.editComplete = true;
+
+      const {
+        arg: { jobId },
+      } = meta;
+
+      if (jobId) {
+        state.jobs.map((job) => (job._id === payload.id ? payload : job));
+      }
+
       state.editItem = payload;
     },
-    [updateJobAsync.rejected]: (state) => {
+    [updateJobAsync.rejected]: (state, { payload }) => {
       state.isLoading = false;
       state.editComplete = true;
       state.showAlert = true;
+      state.error = payload.message;
     },
-    [deleteJobAsync.fulfilled]: (state, { payload }) => {
-      state.jobs = state.jobs.filter((job) => job._id !== payload);
+    [deleteJobAsync.pending]: (state) => {
+      state.isLoading = true;
+    },
+    [deleteJobAsync.fulfilled]: (state, { meta }) => {
+      state.isLoading = false;
+
+      const {
+        arg: { jobId },
+      } = meta;
+
+      if (jobId) {
+        state.jobs = state.jobs.filter((job) => job._id !== jobId);
+      }
+    },
+    [deleteJobAsync.rejected]: (state, { payload }) => {
+      state.isLoading = false;
+      state.error = payload.message;
     },
   },
 });

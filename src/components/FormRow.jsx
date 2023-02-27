80eea9f7dfe0e41e8c2af65f3c619fb6281f0db7
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-02-27 15:57:55 +0100

    fix: change file extension from js to .jsx

---
+++ src/components/FormRow.jsx
@@ -0,0 +1,28 @@
+const FormRow = ({
+  type,
+  name,
+  value,
+  handleChange,
+  horizontal,
+  placeholder,
+}) => {
+  return (
+    <div className='form-row'>
+      {!horizontal && (
+        <label htmlFor={name} className='form-label'>
+          {name}
+        </label>
+      )}
+      <input
+        type={type}
+        value={value}
+        name={name}
+        onChange={handleChange}
+        className='form-input'
+        placeholder={placeholder}
+      />
+    </div>
+  );
+};
+
+export default FormRow;

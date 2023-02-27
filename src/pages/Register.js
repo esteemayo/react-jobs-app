6048a0d1e5cb35633d38b28dcfa7e7d585e32c4a
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-02-27 16:10:33 +0100

    feat: import reset function

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -6,7 +6,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import logo from 'assets/logo.svg';
 import Alert from 'components/Alert';
 import FormRow from 'components/FormRow';
-import { loginUserAsync, registerUserAsync } from 'redux/users/userSlice';
+import { loginUserAsync, registerUserAsync, reset } from 'redux/users/userSlice';
 import { useEffect } from 'react';
 
 const initialState = {

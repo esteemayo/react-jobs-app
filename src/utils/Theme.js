0d5081c4effd75a81c10d759043c57b77ec80d7b
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-02-28 11:46:53 +0100

    fix: add new properties

--- src/utils/Theme.js
+++ src/utils/Theme.js
@@ -1,9 +1,15 @@
 export const darkTheme = {
   bg: '#0d1117',
   bgLighter: '#161b22',
+  text: 'rgb(156, 156, 156)',
+  textSoft: '#aaaaaa',
+  soft: '#373737',
 };
 
 export const lightTheme = {
   bg: 'var(--backgroundColor)',
   bgLighter: 'var(--white)',
+  text: '#777777',
+  textSoft: '#606060',
+  soft: '#f5f5f5',
 };

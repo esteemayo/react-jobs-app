4ef9f42a2dfcc9393f1cea734245200e6f5087a2
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-02-28 01:10:48 +0100

    fix: modify

--- src/pages/Edit.jsx
+++ src/pages/Edit.jsx
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 import { useLocation, Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 import FormRow from 'components/FormRow';

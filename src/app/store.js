750c61e686a42b2d6b4dbe2acb512c72d0b9c0c5
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-02-28 02:49:51 +0100

    fix: modify

--- src/app/store.js
+++ src/app/store.js
@@ -1,7 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
 
 import jobReducer from '../redux/jobs/jobSlice';
-import userReducer from '../redux/users/userSlice';
+import userReducer from 'redux/users/userSlice';
 import darkModeReducer from 'redux/darkMode/darkMode';
 
 const store = configureStore({

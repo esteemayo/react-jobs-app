eda91caecf0d96bf0bf929f2c813eeee73f6efe1
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-02-28 01:31:55 +0100

    fix: modify

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from 'react';
-import styled from 'styled-components';
 import { toast } from 'react-toastify';
 import { useDispatch, useSelector } from 'react-redux';
 

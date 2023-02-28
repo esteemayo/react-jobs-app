f665ca4470ffaa3aa9beae6bc5269c1df0151655
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-02-28 02:49:35 +0100

    feat: add darkModeReducer to redux toolkit store

--- src/app/store.js
+++ src/app/store.js
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit';
 
 import jobReducer from '../redux/jobs/jobSlice';
 import userReducer from '../redux/users/userSlice';
+import darkModeReducer from 'redux/darkMode/darkMode';
 
 const store = configureStore({
   reducer: {
     jobs: jobReducer,
     user: userReducer,
+    darkMode: darkModeReducer,
   },
 });
 

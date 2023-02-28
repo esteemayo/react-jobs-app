a67cace5d977b3aadf98504a1963855131d20c06
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-02-28 02:54:31 +0100

    fix: wrap app with ThemeProvider

--- src/App.js
+++ src/App.js
@@ -18,45 +18,47 @@ function App() {
   const { darkMode } = useSelector((state) => state.darkMode);
 
   return (
-    <Router>
-      <Routes>
-        <Route path='/' element={<SharedLayout />}>
-          <Route
-            index
-            element={
-              <AuthRoute>
-                <Home />
-              </AuthRoute>
-            }
-          />
-          <Route
-            path='dashboard'
-            element={
-              <ProtectedRoute>
-                <Dashboard />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path='register'
-            element={
-              <AuthRoute>
-                <Register />
-              </AuthRoute>
-            }
-          />
-          <Route
-            path='edit/:slug'
-            element={
-              <ProtectedRoute>
-                <Edit />
-              </ProtectedRoute>
-            }
-          />
-        </Route>
-        <Route path='*' element={<Error />} />
-      </Routes>
-    </Router>
+    <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
+      <Router>
+        <Routes>
+          <Route path='/' element={<SharedLayout />}>
+            <Route
+              index
+              element={
+                <AuthRoute>
+                  <Home />
+                </AuthRoute>
+              }
+            />
+            <Route
+              path='dashboard'
+              element={
+                <ProtectedRoute>
+                  <Dashboard />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path='register'
+              element={
+                <AuthRoute>
+                  <Register />
+                </AuthRoute>
+              }
+            />
+            <Route
+              path='edit/:slug'
+              element={
+                <ProtectedRoute>
+                  <Edit />
+                </ProtectedRoute>
+              }
+            />
+          </Route>
+          <Route path='*' element={<Error />} />
+        </Routes>
+      </Router>
+    </ThemeProvider>
   );
 }
 

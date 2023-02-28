03a8826ae154ba7a2d51087018d3c282f4aad768
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-02-28 11:43:08 +0100

    fix: fix a bug

--- src/components/FormRow.jsx
+++ src/components/FormRow.jsx
@@ -9,7 +9,7 @@ const FormRow = ({
   placeholder,
 }) => {
   return (
-    <Container>
+    <Container className='form-row'>
       {!horizontal && (
         <label htmlFor={name} className='form-label'>
           {name}
@@ -28,12 +28,7 @@ const FormRow = ({
 };
 
 const Container = styled.div`
-  width: 100%;
-  padding: 0.375rem 0.75rem;
-  border-radius: var(--borderRadius);
-  background: ${({ theme }) => theme.bg};
-  border: 1px solid var(--grey-200);
-  caret-color: var(--primary-500);
+  
 `;
 
 export default FormRow;

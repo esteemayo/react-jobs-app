1b985aab070a48b33a33ef3161021bdda7d541e8
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-02-28 02:11:40 +0100

    feat: add setToStorage function

--- src/utils/index.js
+++ src/utils/index.js
@@ -3,3 +3,7 @@ export const tokenKey = 'jwt';
 export const getFromStorage = (key) => {
   return JSON.parse(localStorage.getItem(key));
 }
+
+export const setToStorage = (key, value) => {
+  return localStorage.setItem(key, JSON.stringify(value));
+}

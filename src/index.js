274ce152820ce102732d63d422304f8acc11be08
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-02-28 01:39:00 +0100

    fix: modify

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import 'normalize.css';
-import './index.css';
 import App from './App';
 import store from './app/store';
 import reportWebVitals from './reportWebVitals';

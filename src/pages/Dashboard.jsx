1bcc9537c2a55c9b2d7adf0a718f402475c12701
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-02-28 01:32:25 +0100

    fix: modify

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -4,7 +4,7 @@ import { toast } from 'react-toastify';
 import { useDispatch, useSelector } from 'react-redux';
 
 import Alert from 'components/Alert';
-import FormRow from 'components/FormRow';
+import Jobs from 'components/Jobs';
 import { createJob, reset } from 'redux/jobs/jobSlice';
 
 const initialState = {

b3013980e5fb75be21674006b64a36f51e683cbf
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-03-02 02:18:03 +0100

    fix: modify

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -7,7 +7,7 @@ const Error = () => {
   return (
     <Wrapper className='page full-page'>
       <div>
-        <img src={img} alt='not found' />
+        <img src={img} alt='' />
         <h3>Ohh! page not found</h3>
         <p>We can't seem to find the page you're looking for</p>
         <Link to='/'>Back to home</Link>

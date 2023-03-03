786db85a9198c1865ea98f7d4dc7eaa2eeb75323
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-03-03 03:50:38 +0100

    fix: modify

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -67,7 +67,6 @@ const Wrapper = styled.nav`
   
       &:hover {
         background-color: ${({ theme }) => theme.bgBtnHover};
-        box-shadow: ${({ theme }) => theme.boxBtn};
       }
     }
   }

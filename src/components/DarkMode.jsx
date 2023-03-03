5124926675ecb5ba429dc1f0231610fb5944d1a3
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-03-03 01:06:13 +0100

    fix: modify

--- src/components/DarkMode.jsx
+++ src/components/DarkMode.jsx
@@ -17,7 +17,7 @@ const Container = styled.div`
   justify-content: center;
   border-top-right-radius: 2rem;
   border-bottom-right-radius: 2rem;
-  box-shadow: 0px 0px 4px rgba(0, 0, 0, 0.1);
+  box-shadow: 0px 0px 4px rgba(255, 255, 255, 0.5);
 `;
 
 const Wrapper = styled.div`

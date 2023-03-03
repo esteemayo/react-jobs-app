72d74157955a0f5ab7d6c4c158e62975cb2c86ff
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-03-03 03:13:20 +0100

    fix: modify

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 import { useState } from 'react';
-import { FaUserCircle, FaCaretDown, FaRegMoon } from 'react-icons/fa';
+import {FaCaretDown, FaUserCircle, FaRegMoon } from 'react-icons/fa';
 import { useDispatch, useSelector } from 'react-redux';
 import { BiSun } from 'react-icons/bi';
 
